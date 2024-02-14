578159677118f4449a72f8764fde98c6401fdbea
stevaughn borders <150064082+stevaughnborders@users.noreply.github.com>
2024-02-14 14:34:38 -0600

    Add files via upload

---
+++ Module 4 project 1.py
@@ -0,0 +1,25 @@
+print("What is everyone plans this weekend?")
+print("1.Are you going out with family?")
+print("2.are you going out drinking?")
+print("3.are you going to a football game?")
+print("4.are you planning on leaving town?")
+print("5.will you be with your significant other?")
+print("6.Are you going out for valentines day?")
+print("0.exit")
+Number=input("what number do you use ?")
+if Number=="1":
+    print("I love being with family")
+elif Number=="2":
+    print("Not this weekend")
+elif Number=="3":
+    print("yes I went to watch the superbowl")
+elif Number=="4":
+    print("Yes I went to las vegas")
+elif Number=="5":
+    print("Yes she is with me now")
+elif Number=="6":
+    print("las vegas was for valentines day")
+elif Number=="0":
+    print("goodbye")
+else:
+    print("invalid option")
